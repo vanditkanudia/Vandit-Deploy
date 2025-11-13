c77b90deb4909046c45ea4d5d3db67bb493d18de
vanditkanudia <vandit@kanors.com>
2025-11-13 13:21:20 +0530

    Rename patch folder and update output file name

--- run_batch.bat
+++ run_batch.bat
@@ -1,12 +1,11 @@
 @echo off
 REM ===1. Set the folder name ===
-set "TARGET_DIR=D:\GitHub\Patch_Folder"
+set "TARGET_DIR=D:\GitHub\Batch_Folder"
 
 REM ===2. Create the folder if it doesn't exist ===
 if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%"
 
 REM ===3. Write current Date and Time into a text file ===
-echo %DATE% %TIME% > "%TARGET_DIR%\patch.txt"
+echo %DATE% %TIME% > "%TARGET_DIR%\batch.txt"
 
-echo Patch script finished
-pause
+echo Batch script finished
